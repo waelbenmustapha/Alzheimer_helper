411b8071697c41629ca60c7323cb553fd23626cb
Elaa Mannai <50701749+elaa-mannai@users.noreply.github.com>
2022-02-20 21:09:14 +0100

    first-version

--- Front/App.js
+++ Front/App.js
@@ -1,14 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import CheckNotes from './Src/Views/CheckNotes';
+import CheckNote from './Src/Views/CheckNote';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to ALZH</Text>
       <CheckNotes/>
-<Text>save for github</Text>
-      
       <StatusBar style="auto" />
     </View>
   );
@@ -18,7 +16,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
 });
