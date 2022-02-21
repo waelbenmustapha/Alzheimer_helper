3302d193c8b4f199c28f520c29e829e90a16ee7a
MariemTanazefti <tanazeftimariem9@gmail.com>
2022-02-21 08:55:10 +0100

    all good

--- Front/App.js
+++ Front/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AddNotes from './Src/Views/AddNotes';
 import CheckNotes from './Src/Views/CheckNotes';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Welcome to ALZH</Text>
+      <AddNotes/>
       <CheckNotes/>
 
       
