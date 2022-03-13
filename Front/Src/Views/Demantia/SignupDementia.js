da1c33281842acebd69bbaea6d62b99a05527c9d
zzsliti1999 <zzsliti1999@gmail.com>
2022-03-13 23:24:47 +0100

    zaeaze

--- Front/Src/Views/Demantia/SignupDementia.js
+++ Front/Src/Views/Demantia/SignupDementia.js
@@ -47,7 +47,7 @@ const SignupDementia = ({ navigation }) => {
     if (isValid ()){
    setIsLoading(true);
 
-    axios.post(`http://172.16.23.165:8090/dementia/SignUp/${guardianEmail}`, {
+    axios.post(`http://192.168.1.16:8090/dementia/SignUp/${guardianEmail}`, {
       name: userName,
       email: userEmail,
       password: userPassword,
